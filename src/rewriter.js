718b452d8fc83480a1c52e0ed1eded0f63751fff
cake-wakame <pl46@outlook.jp>
2025-11-25 22:26:02 +0900

    Update rewriter.js

--- src/rewriter.js
+++ src/rewriter.js
@@ -9,17 +9,17 @@ const rewriteUrl = (url, baseUrl) => {
 
 const rewriteHtml = (html, baseUrl) => {
   let rewritten = html;
-  
+
   rewritten = rewritten.replace(
     /(<a\s+[^>]*href=["'])([^"']+)(["'][^>]*>)/gi,
     (match, p1, p2, p3) => `${p1}${rewriteUrl(p2, baseUrl)}${p3}`
   );
-  
+
   rewritten = rewritten.replace(
     /(<img\s+[^>]*src=["'])([^"']+)(["'][^>]*>)/gi,
     (match, p1, p2, p3) => `${p1}${rewriteUrl(p2, baseUrl)}${p3}`
   );
-  
+
   rewritten = rewritten.replace(
     /(<img\s+[^>]*srcset=["'])([^"']+)(["'][^>]*>)/gi,
     (match, p1, p2, p3) => {
@@ -31,62 +31,62 @@ const rewriteHtml = (html, baseUrl) => {
       return `${p1}${rewrittenSrcset}${p3}`;
     }
   );
-  
+
   rewritten = rewritten.replace(
     /(<link\s+[^>]*href=["'])([^"']+)(["'][^>]*>)/gi,
     (match, p1, p2, p3) => `${p1}${rewriteUrl(p2, baseUrl)}${p3}`
   );
-  
+
   rewritten = rewritten.replace(
     /(<script\s+[^>]*src=["'])([^"']+)(["'][^>]*>)/gi,
     (match, p1, p2, p3) => `${p1}${rewriteUrl(p2, baseUrl)}${p3}`
   );
-  
+
   rewritten = rewritten.replace(
     /(<iframe\s+[^>]*src=["'])([^"']+)(["'][^>]*>)/gi,
     (match, p1, p2, p3) => `${p1}${rewriteUrl(p2, baseUrl)}${p3}`
   );
-  
+
   rewritten = rewritten.replace(
     /(<form\s+[^>]*action=["'])([^"']+)(["'][^>]*>)/gi,
     (match, p1, p2, p3) => `${p1}${rewriteUrl(p2, baseUrl)}${p3}`
   );
-  
+
   rewritten = rewritten.replace(
     /(<video\s+[^>]*src=["'])([^"']+)(["'][^>]*>)/gi,
     (match, p1, p2, p3) => `${p1}${rewriteUrl(p2, baseUrl)}${p3}`
   );
-  
+
   rewritten = rewritten.replace(
     /(<audio\s+[^>]*src=["'])([^"']+)(["'][^>]*>)/gi,
     (match, p1, p2, p3) => `${p1}${rewriteUrl(p2, baseUrl)}${p3}`
   );
-  
+
   rewritten = rewritten.replace(
     /(<source\s+[^>]*src=["'])([^"']+)(["'][^>]*>)/gi,
     (match, p1, p2, p3) => `${p1}${rewriteUrl(p2, baseUrl)}${p3}`
   );
-  
+
   rewritten = rewritten.replace(
     /(<source\s+[^>]*srcset=["'])([^"']+)(["'][^>]*>)/gi,
     (match, p1, p2, p3) => `${p1}${rewriteUrl(p2, baseUrl)}${p3}`
   );
-  
+
   rewritten = rewritten.replace(
     /(<picture\s+[^>]*src=["'])([^"']+)(["'][^>]*>)/gi,
     (match, p1, p2, p3) => `${p1}${rewriteUrl(p2, baseUrl)}${p3}`
   );
-  
+
   rewritten = rewritten.replace(
     /(<meta\s+[^>]*content=["'])(\d+;\s*url=)([^"']+)(["'][^>]*>)/gi,
     (match, p1, p2, p3, p4) => `${p1}${p2}${rewriteUrl(p3, baseUrl)}${p4}`
   );
-  
+
   rewritten = rewritten.replace(
     /(\sdata-(?:src|background|lazy|original|url|image|poster|thumbnail|href)[^=]*=["'])([^"']+)(["'])/gi,
     (match, p1, p2, p3) => `${p1}${rewriteUrl(p2, baseUrl)}${p3}`
   );
-  
+
   rewritten = rewritten.replace(
     /(url\(["']?)([^"')]+)(["']?\))/gi,
     (match, p1, p2, p3) => {
@@ -94,12 +94,12 @@ const rewriteHtml = (html, baseUrl) => {
       return `${p1}${rewriteUrl(p2, baseUrl)}${p3}`;
     }
   );
-  
+
   rewritten = rewritten.replace(
     /(<base\s+[^>]*href=["'])([^"']+)(["'][^>]*>)/gi,
     ''
   );
-  
+
   return rewritten;
 };
 
